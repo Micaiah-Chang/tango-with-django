71d8959e9e062f95cede45175f5b731a7035180d
Micaiah-Chang <Micaiah@server.fake>
2014-07-14 00:03:12 -0700

    Add ability to populate and test database

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,18 +3,39 @@ from django.conf.urls import url
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-def index(request):
-    # Request the context of the request.  
-    # The context contains information such as the client's machine details, for example.
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
+
+
+def add_category(request):
     context = RequestContext(request)
 
-    # Constuct a dictionary to pass to the template engine as its context
-    # Note the key boldmessage is the same as  {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = CategoryForm()
+
+    return render_to_response('rango/add_category.html', {'form' : form}, context)
+
+
+def index(request):
+    context = RequestContext(request)
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    
+    context_dict = {'categories' : category_list,
+                    'pages' : page_list}
+
+    for category in category_list:
+        category.url = category.name.replace(' ','_')
+    
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
@@ -22,3 +43,23 @@ def about(request):
     context_dict = {}
     
     return render_to_response('rango/about.html', context_dict, context)
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+
+    category_name = category_name_url.replace('_', ' ')
+
+    context_dict = {'category_name' : category_name}
+
+    try:
+        category = Category.objects.get(name=category_name)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
