f4048381e20786f35f24d1e6600c88e8980a11fa
Micaiah-Chang <Micaiah@server.fake>
2014-07-14 17:51:56 -0700

    Page addition to category functionality implemented.

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -11,7 +11,7 @@ class CategoryForm(forms.ModelForm):
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200,help_text="Pleae enter the URL of the page.")
+    url = forms.URLField(max_length=200,help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:     
@@ -19,7 +19,7 @@ class PageForm(forms.ModelForm):
         fields = ('title', 'url', 'views')
         
     def clean(self):
-        cleaned_data = self.claned_data
+        cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
 
         if url and not url.startswith('http://'):
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,6 +6,43 @@ from django.shortcuts import render_to_response
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm
 
+def add_page(request, category_name_url):
+    context = RequestContext(request)
+
+    category_name = decode_url(category_name_url)
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            page = form.save(commit=False)
+
+            # Automatically add to proper category unless...
+            # Category does not exist, in which case, ask the user to add it.
+            try:
+                cat = Category.objects.get(name=category_name)
+                page.category = cat
+            except Category.DoesNotExist:
+                return render_to_response('rango/add_category.html', {}, context)
+
+            page.views = 0
+
+            page.save()
+            # After done sending form, send user to category page
+            # To show that their changes went through
+            return category(request, category_name_url)
+
+        else:
+            # Invalid form needs to tell user what about it is invalid
+            print form.errors
+    else:
+        form = PageForm()
+
+    return render_to_response('rango/add_page.html',
+                              {'category_name_url': category_name_url,
+                               'category_name' : category_name,
+                              'form': form},
+                              context)
+
 
 def add_category(request):
     context = RequestContext(request)
@@ -49,7 +86,8 @@ def category(request, category_name_url):
 
     category_name = category_name_url.replace('_', ' ')
 
-    context_dict = {'category_name' : category_name}
+    context_dict = {'category_name' : category_name,
+                    'category_name_url': category_name_url}
 
     try:
         category = Category.objects.get(name=category_name)
@@ -63,3 +101,11 @@ def category(request, category_name_url):
         pass
 
     return render_to_response('rango/category.html', context_dict, context)
+
+def decode_url(url):
+    return url.replace('_', ' ')
+
+def encode_url(url):
+    return ulr.replace(' ', '_')
+    
+    
